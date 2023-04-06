f29d49230ccb7cc7648574d9bd5578df26709ff3
hr-virieux <105490926+hr-virieux@users.noreply.github.com>
2023-04-06 12:01:46 +1000

    Create ASCII art.cs

---
+++ learn_cs/ASCII art.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+//printing ASCII shapes on console
+      Console.WriteLine(" .-.");    
+      Console.WriteLine("(o o)");   
+      Console.WriteLine("| O |");   
+      Console.WriteLine("'~~~'");  
+      Console.WriteLine("It's a ghost");  
+      Console.WriteLine(); 
+
+//printing ASCII shapes on console Dancing man
+      Console.WriteLine("c_     ___");    
+      Console.WriteLine("　 \\\\__|__|__");    
+      Console.WriteLine("　　 \\( o_o)");   
+      Console.WriteLine("　　　 > ~  >");     
+      Console.WriteLine("　　　/ 　 / \\\\");   
+      Console.WriteLine("　　 /　　/　 \\\\");   
+      Console.WriteLine("　　 \\　 )　　c/");   
+      Console.WriteLine("　　/　 /");   
+      Console.WriteLine("　 /　/|");   
+      Console.WriteLine("　(　( \\");   
+      Console.WriteLine("　|　|  \\");   
+      Console.WriteLine("　| / \\  )");   
+      Console.WriteLine("　| |　) |");   
+      Console.WriteLine(" /  )(_/");   
+      Console.WriteLine("(_ /");   
+      Console.WriteLine("Dancing Man");
+      Console.WriteLine();   
+
+//Ascii characters Angry Volcano
+      Console.WriteLine("     (    )");   
+      Console.WriteLine("  (  (   )  )");   
+      Console.WriteLine("(  (       )  )");   
+      Console.WriteLine("  (  (   )    )");   
+      Console.WriteLine("     (     )");   
+      Console.WriteLine("    /--------\\\\");   
+      Console.WriteLine("   /   \\\\   /  \\\\");   
+      Console.WriteLine("  /     0 0    \\\\");   
+      Console.WriteLine(" /       o      \\\\");   
+      Console.WriteLine(" ****************");   
+      Console.WriteLine("Angry Volcano");
+      Console.WriteLine();  
+
+//Ascii characters Super Bug
+      Console.WriteLine("   /   \\");   
+      Console.WriteLine("   \\. ./");   
+      Console.WriteLine("  (o + o)");   
+      Console.WriteLine("--|  |  |--");   
+      Console.WriteLine("--|  |  |--");   
+      Console.WriteLine("--|  |  |--");   
+      Console.WriteLine("   v   v");   
+      Console.WriteLine("   *****");   
+      Console.WriteLine("Super Bug");
+      Console.WriteLine();  
+     }
+  }
+}
